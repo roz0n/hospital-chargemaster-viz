81ba0fc1368035e325ca7bfafa36622675764f3e
Arnold Rozon <arnold@rozon.org>
2020-05-07 20:01:43 -0500

    Knex cleanup and npm audit

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const dataSet = require("./data/hospitals.json");
 const db = require("./db/knex");
 const app = express();
@@ -16,26 +14,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.get("/api", (req, res) => res.send({ success: true, data: new Date() }));
-
-async function assertDatabaseConnection() {
-  return db.raw("select 1+1 as result").catch((err) => {
-    console.log(
-      "[Fatal] Failed to establish connection to database! Exiting..."
-    );
-    console.log(err);
-    process.exit(1);
-  });
-}
-
 app.get("/test", async (req, res) => {
   try {
     const query = await db("hospitals").where({ name: "Columbia" });
 
     if (query) {
-      console.log("Query response", query);
       res.send({ success: true, data: query })
     } else {
       throw new Error("Could not query db");
